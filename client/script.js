56e50ca9c3e4cda5a36ca0a73f4b63074e6520b3
adnanali <alkholaidi@verizon.net>
2022-12-25 15:25:39 -0500

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:8000', {
+  const response = await fetch('https://codex-uwft.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -115,4 +115,4 @@ form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
-});
+});   
